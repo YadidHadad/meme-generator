32d0f138777255d3a0fdab87c96a984c99e860b9
Yadid Hadad <yadidhadad@gmail.com>
2022-10-26 18:03:19 +0300

    build gallery

---
+++ js/memeController.js
@@ -0,0 +1,144 @@
+'use strict'
+
+
+let gTextStrokeColor = '#000000'
+let gTextFillColor = '#FFFFFF'
+let gTextSize = '2rem'
+let gTextFamily = 'Arial'
+let gTextAlign = 'center'
+
+let gElCanvas = {}
+let gCtx
+
+function onInit() {
+
+    gElCanvas = resizeCanvas()
+    gElCanvas = document.querySelector('#canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    renderGallery()
+    addListeners()
+}
+
+
+function renderMeme(elImg, id) {
+    // get meme data from service
+    const { selectedImgId, selectedLineIdx, lines } = getMeme()
+
+    console.log(elImg)
+
+    setMemeImg(id)
+
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText(lines[selectedLineIdx].txt, gElCanvas.width / 2, 10)
+}
+
+// RESIZE CANVAS
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    // console.log(`elContainer:`, elContainer)
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
+
+// CANVAS ACTIONS
+// IMG
+function drawImg(src) {
+    const elImg = document.querySelector('.current-meme-img')
+    elImg.src = src
+    // const img = document.querySelector('img')
+    // Naive approach:
+    // there is a risk that image is not loaded yet and nothing will be drawn on canvas
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height) // Draws the specified image
+}
+
+// TEXT
+function drawText(text, x, y) {
+    console.log(text)
+    console.log(x, y)
+    gCtx.textBaseline = "top";
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = gTextStrokeColor
+    gCtx.fillStyle = gTextFillColor
+    gCtx.textAlign = 'center';
+    gCtx.font = `${gTextSize} ${gTextFamily}`
+    gCtx.fillText(text, x, y) // Draws (fills) a given text at the given (x, y) position.
+    gCtx.strokeText(text, x, y) // Draws (strokes) a given text at the given (x, y) position.
+}
+
+
+// EVENT LISTENERES
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+    //Listen for resize ev 
+    window.addEventListener('resize', () => {
+        resizeCanvas()
+        renderCanvas()
+    })
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+// EVENTS
+function onDown(ev) {
+    console.log('Im from onDown')
+    //Get the ev pos from mouse or touch
+    const pos = getEvPos(ev)
+    if (!isCircleClicked(pos)) return
+    setCircleDrag(true)
+    //Save the pos we start from 
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+
+}
+
+function onMove(ev) {
+    console.log('Im from onMove')
+    const { isDrag } = getCircle()
+    if (!isDrag) return
+    const pos = getEvPos(ev)
+    //Calc the delta , the diff we moved
+    const dx = pos.x - gStartPos.x
+    const dy = pos.y - gStartPos.y
+    moveCircle(dx, dy)
+    //Save the last pos , we remember where we`ve been and move accordingly
+    gStartPos = pos
+    //The canvas is render again after every move
+    renderCanvas()
+
+}
+
+function onUp() {
+    console.log('Im from onUp')
+    setCircleDrag(false)
+    document.body.style.cursor = 'grab'
+}
+
+// RENDER GALLERY
+
+function renderGallery() {
+
+    const imgsGallery = document.querySelector('.grid-container')
+    const imgs = getImages()
+
+    const strHTML = imgs.map((img) => {
+        return `
+                <img id="${img.id}" class="grid-item" current-meme-img" src=${img.url} alt="" onClick="renderMeme(this, ${img.id})">`
+    })
+
+    imgsGallery.innerHTML = strHTML.join('')
+    console.log(strHTML.join(''))
+
+}
--- js/service/meme.service.js
+++ js/service/meme.service.js
@@ -1,18 +1,63 @@
 'use strict'
 
-var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2, 'happy': 1, 'crazy': 1, 'sarcastic': 1, 'sad': 1, 'animal': 1 }
 
-var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }];
+var gImgs = [
+    { id: 1, url: './img/meme-imgs/1.jpg', keywords: ['funny', 'politics'] },
+    { id: 2, url: './img/meme-imgs/2.jpg', keywords: ['dog', 'puppy', 'cute', 'animal'] },
+    { id: 3, url: './img/meme-imgs/3.jpg', keywords: ['dog', 'puppy', 'cute', 'baby', 'animal'] },
+    { id: 4, url: './img/meme-imgs/4.jpg', keywords: ['kitten', 'cat', 'cute', 'animal'] },
+    { id: 5, url: './img/meme-imgs/5.jpg', keywords: ['baby', 'sarcastic'] },
+    { id: 6, url: './img/meme-imgs/6.jpg', keywords: ['arcastic', 'funny'] },
+    { id: 7, url: './img/meme-imgs/7.jpg', keywords: ['baby', 'cute', 'shock'] },
+    { id: 8, url: './img/meme-imgs/8.jpg', keywords: ['sarcastic', 'funny'] },
+    { id: 9, url: './img/meme-imgs/9.jpg', keywords: ['sarcastic', 'baby', 'cute', 'funny'] },
+    { id: 10, url: './img/meme-imgs/10.jpg', keywords: ['funny', 'politics'] },
+    { id: 11, url: './img/meme-imgs/11.jpg', keywords: ['gay', 'sarcastic', 'love', 'funny'] },
+    { id: 12, url: './img/meme-imgs/12.jpg', keywords: ['sarcastic', 'funny'] },
+    { id: 13, url: './img/meme-imgs/13.jpg', keywords: ['happy', 'toast', 'drink', 'sarcastic'] },
+    { id: 14, url: './img/meme-imgs/14.jpg', keywords: ['sarcastic', 'crazy'] },
+    { id: 15, url: './img/meme-imgs/15.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 16, url: './img/meme-imgs/16.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 17, url: './img/meme-imgs/17.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 18, url: './img/meme-imgs/18.jpg', keywords: ['funny', 'toys', 'cute'] },
+
+];
 
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [
         {
-            txt: 'I sometimes eat Falafel',
+            txt: '',
             size: 20,
-            align: 'left',
-            color: 'red'
+            align: 'center',
+            color: 'white',
+            strokeColor: 'black',
         }
     ]
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getImg(id) {
+
+    return gImgs.find((img) => img.id === id)
+
+}
+
+function getImages() {
+    return gImgs
+
+}
+
+function setMemeImg(id) {
+    gMeme.selectedImgId = id
+
+    const img = getImg(id)
+
+    console.log(img)
+
 }
